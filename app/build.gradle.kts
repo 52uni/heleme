681bbe9ec75800c0f85c3e0445123bd50274418e
52uni <749054429@qq.com>
2026-07-23 15:43:01 +0800

    v1.1 - 检查更新按钮移到侧边栏底部

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.zhengui.waterreminder"
         minSdk = 24
         targetSdk = 34
-        versionCode = 100
-        versionName = "1.0"
+        versionCode = 101
+        versionName = "1.1"
     }
 
     buildTypes {
